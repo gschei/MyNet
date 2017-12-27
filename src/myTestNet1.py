072d6e11dfdd9da09a01b8e159b2e1d07a087e30
Gilber Scheiblhofer <gilbert.scheiblhofer@a1telekom.at>
2017-12-27 23:00:08 +0100

    continued

--- src/myTestNet1.py
+++ src/myTestNet1.py
@@ -2,7 +2,9 @@ from net import Net
 
 
 n = Net()
-n.read_testdata('/home/gilbert/proj/sc2dm/pysc2/testdata.txt')
-n.initializeWeights(2)
 #n.testCalculation()
-n.train(2, 3, 6)
+
+n.read_testdata('/home/gilbert/proj/sc2dm/pysc2/testdata.txt')
+n.initializeWeights(2,3,1)
+n.query(2, 3)
+
--- src/net.py
+++ src/net.py
@@ -21,20 +21,35 @@ class Net:
             i+=1
 
 
-    def train(self, p1, p2, result ):
-        minput=[]
-        minput.append(p1)
-        minput.append(p2)
+    def scale(self, x):
+        return ((x / 1000) * 0.99) + 0.01
 
-        # layer 1
+    def descale(self, x):
+        return ((x - 0.01) * 1000) / 0.99
+
+
+    def train(self, p1, p2, result ):
+        pass
+
+    def query(self, p1, p2):
+        minput=[]
+        xoutput=[]
+        minput.append(self.scale(p1))
+        minput.append(self.scale(p2))
+
+        # node layer
         layer1 = self.calculateX(minput,self.weight1)
         layer1_sig = self.addSigmoid(layer1)
 
-        # layer 2
         layer2 = self.calculateX(layer1_sig,self.weight2)
         layer2_sig = self.addSigmoid(layer2)
 
-        print ("output training 1 "+str(layer2_sig))
+        for i in range (0,len(layer2_sig)):
+            xoutput.append(self.descale(layer2_sig[i]))
+
+        print ("output query "+str(xoutput))
+
+        return xoutput
 
 
 
@@ -49,10 +64,15 @@ class Net:
 
         moutput = []
 
-        for i in range(0,len(inputs)):
+        print("input: "+str(inputs))
+        print("weights: "+str(weights))
+
+        for i in range(0,len(weights)):
             x = 0
+            #print("  outer: "+str(i))
             for j in range (0,len(inputs)):
-                x = x + inputs[j] * weights[j][i]
+                #print("  inner: i "+str(i)+" - j "+str(j))
+                x = x + inputs[j] * weights[i][j]
 
             moutput.append(x)
 
@@ -69,22 +89,22 @@ class Net:
     def testCalculation(self):
         print("----- do test calcultion -----")
 
-        self.initializeWeights(3)
+        self.initializeWeights(3,3,3)
 
         self.minput.append(0.9)
         self.minput.append(0.1)
         self.minput.append(0.8)
 
         self.weight1[0][0] = 0.9
-        self.weight1[0][1] = 0.2
-        self.weight1[0][2] = 0.1
+        self.weight1[1][0] = 0.2
+        self.weight1[2][0] = 0.1
 
-        self.weight1[1][0] = 0.3
+        self.weight1[0][1] = 0.3
         self.weight1[1][1] = 0.8
-        self.weight1[1][2] = 0.5
+        self.weight1[2][1] = 0.5
 
-        self.weight1[2][0] = 0.4
-        self.weight1[2][1] = 0.2
+        self.weight1[0][2] = 0.4
+        self.weight1[1][2] = 0.2
         self.weight1[2][2] = 0.6
 
         print("starting with: ")
@@ -101,17 +121,28 @@ class Net:
 
     def initializeWeights(self, numInputs, numNodes, numOutputs):
 
+        #  this creates a two dimensional array, where
+        #  - the number of entries in weights1 is the number of Nodes
+        #  - the number of entries in each sub-array is the number of Inputs
+
+
         for i in range(0,numNodes):
             if (i>0):
               self.weight1.append([])
-            for j in range(0,numNodes):
+            for j in range(0,numInputs):
                 self.weight1[i].append(random())
 
         print(self.weight1)
 
-  a
-x b
-y c
+        for i in range(0,numOutputs):
+            if (i>0):
+                self.weight2.append([])
+            for j in range(0,numNodes):
+                self.weight2[i].append(random())
+
+        print(self.weight2)
+
+
 
 
 
